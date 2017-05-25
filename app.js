a87206d0964eb4aa6a3d5aff5d26e46010903875
JakeRatliff <wrjake@gmail.com>
2017-05-25 17:23:15 -0400

    db store reference

--- app.js
+++ app.js
@@ -28,14 +28,20 @@ console.log("environment = " + environment);
 
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
- 
-   
-var store = new MongoDBStore(
+var store; 
+if(environment == "production"){
+	store = new MongoDBStore(
+    {
+		uri: URI,
+        collection: 'sessions'
+    });
+}else{
+	store = new MongoDBStore(
     {
 		uri: 'mongodb://localhost:27017/connect_mongodb_session_test' || URI,
         collection: 'sessions'
     });
- 
+}; 
     // Catch errors 
 store.on('error', function(error) {
     console.log(error);
