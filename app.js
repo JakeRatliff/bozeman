b1eb2d2574f24a34fa49d8d26180804a7bed4a48
JakeRatliff <wrjake@gmail.com>
2017-05-25 17:19:00 -0400

    removed assert

--- app.js
+++ app.js
@@ -38,8 +38,7 @@ var store = new MongoDBStore(
  
     // Catch errors 
 store.on('error', function(error) {
-    assert.ifError(error);
-    assert.ok(false);
+    console.log(error);
 });
  
 app.use(require('express-session')({
